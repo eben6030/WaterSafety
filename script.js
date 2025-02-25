a318236960a453155b2a08633c0c6de9136caa70
eben6030 <150095803+eben6030@users.noreply.github.com>
2025-02-25 14:18:37 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    const button = document.querySelector(".learn-more");
-    button.addEventListener("click", function () {
-        alert("Water safety is crucial! Always stay vigilant.");
-    });
-});
